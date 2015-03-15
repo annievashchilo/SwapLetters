1a428fa9c58a54fd404971aa55ca27129f26fe4a
idancie <melanthium@gmail.com>
2015-03-15 16:06:20 +0300

    Add "Hello, World" print

---
+++ src/main/runner/Runner.java
@@ -0,0 +1,12 @@
+package main.runner;
+
+import reader.FileReader;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		FileReader fr = new FileReader();
+		fr.printHello();
+	}
+
+}
---
+++ src/reader/FileReader.java
@@ -0,0 +1,12 @@
+package reader;
+
+public class FileReader {
+
+	public FileReader() {
+		
+	}
+
+	public void printHello() {
+		System.out.println("Hello, world!");
+	}
+}
