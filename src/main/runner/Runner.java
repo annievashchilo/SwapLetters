0b5eb8379e650191cd19d996a059774c5238d09e
idancie <melanthium@gmail.com>
2015-03-17 11:26:47 +0300

    Add complete file parser

--- src/main/runner/Runner.java
+++ src/main/runner/Runner.java
@@ -1,12 +1,23 @@
 package main.runner;
 
-import reader.FileReader;
+import reader.Dataset;
 
 public class Runner {
 
 	public static void main(String[] args) {
-		FileReader fr = new FileReader();
-		fr.printHello();
+		Dataset d = new Dataset(args[0]);
+		for (String s : d.fileContent)
+		{
+			System.out.println(s);
+		}
+
+		System.out.println("=============================");
+
+		d.swapChars();
+		for (String s : d.fileContent)
+		{
+			System.out.println(s);
+		}
 	}
 
 }
